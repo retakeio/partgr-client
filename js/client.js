38349631c49fd6e56beedff8aae480a7fe09d5b4
Alex Eftimie <alex@eftimie.ro>
2013-04-27 21:48:17 +0100

    "ți-am băgat-o toată." "după ce ai insistat un pic, ți-a mers"

--- js/client.js
+++ js/client.js
@@ -9,9 +9,10 @@ function sendImage () {
     var c = document.getElementById("canvas");
     var ctx = c.getContext("2d");
     var data;
+    var img = document.getElementById('theImage');
     c.width = img.width;
     c.height = img.height;
-    ctx.drawImage(img,10,10);
+    ctx.drawImage(img, 0, 0, c.width, c.height);
     data = canvas.toDataURL();
     sendMsg(data);
 }
